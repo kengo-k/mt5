b84dad1da4c1c1b39b17b9c9ba33a7f215e48f84
kurobanekengo <kurobanekengo@gmail.com>
2022-06-09 11:29:18 +0900

    MT5Lib: c#用DLLプロジェクト追加

---
+++ DllSources/MT5Lib/source/MT5Lib.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MT5Lib
+{
+    public class DateUtil
+    {
+        public static string GetCurrentDate(string format)
+        {
+            DateTime dt = DateTime.Now;
+            string result = dt.ToString(format);
+            return result;
+        }
+
+        public static string GetCurrentDate()
+        {
+            return DateUtil.GetCurrentDate("yyyy-MM-dd_HHmm");
+        }
+    }
+}
